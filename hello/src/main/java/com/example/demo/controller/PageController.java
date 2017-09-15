786e72de2b551a1f3336e1d774078132b0f00e62
laydianti <laydianti.nadya@gmail.com>
2017-09-15 17:18:34 +0700

    Tutorial 2 akhirnya kelar sip mantap

---
+++ hello/src/main/java/com/example/demo/controller/PageController.java
@@ -0,0 +1,48 @@
+package com.example.demo.controller;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class PageController {
+	
+	@RequestMapping("/hello")
+	public String hello ()
+	{
+		return "hello123";
+	}
+	
+	@RequestMapping("/greeting")
+	public String greeting (@RequestParam(value = "name", required = false, defaultValue = "dunia") String name, Model model)
+	{
+		model.addAttribute("name",name);
+		return "greeting";
+	}
+	
+	@RequestMapping("/greeting/{name}")
+	public String greetingPath (@PathVariable Optional<String> name, Model model)
+	{
+		if (name.isPresent()) {
+			model.addAttribute("name", name.get());
+		} else {
+			model.addAttribute("name", "dengklek");
+		}
+		return "greeting";
+	}
+	
+	@RequestMapping("/perkalian")
+	public String perkalian (@RequestParam(value= "a", required = false, defaultValue = "0") int a, @RequestParam(value= "b", required = false, defaultValue = "0") int b, Model model)
+	{
+		model.addAttribute("a", a);
+		model.addAttribute("b", b);
+		model.addAttribute("result", a*b);
+		return "perkalian";
+	}
+	
+
+}
